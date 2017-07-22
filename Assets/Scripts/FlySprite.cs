93891d4cb68b6f4983dd708b50c347a1d6dba2b2
SyathirRhaido <pratamaridho10@gmail.com>
2017-07-23 06:25:42 +0700

    alpha-01

---
+++ Assets/Scripts/FlySprite.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlySprite : MonoBehaviour {
+
+	private float karakterForce = 10.5f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		bool karakterActive = Input.GetButton ("Fire1");
+
+		if(karakterActive){
+
+			GetComponent<Rigidbody2D>().AddForce (new Vector2(0, karakterForce));
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speedMove;
+
+	private Rigidbody2D myRB;
+
+	private Animator myAnim;
+
+	bool flag;
+
+
+//	public float lastTouchTime, currentTouchTime;
+
+	public float velocityValue, torqueValue, thresholdTime;
+
+	void Awake(){
+		velocityValue = 1.0f;
+		torqueValue = 200.0f;
+		thresholdTime = 0.3f;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		myRB = GetComponent<Rigidbody2D> ();
+
+		myAnim = GetComponent<Animator> ();
+
+		flag = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		MovePlayer ();
+	
+	}
+
+	void MovePlayer(){
+
+		Vector3 currentPos, touchedPos, distanceVec;
+		 if(Input.GetMouseButtonUp(0)){
+//			Debug.Log ("touch");
+			currentPos = Camera.main.WorldToScreenPoint (transform.position);
+			touchedPos = Input.mousePosition;
+			distanceVec = (touchedPos - currentPos).normalized;
+			stopRotatingCatAndMoveIt(distanceVec, velocityValue);
+		}
+	}
+		
+
+	void stopRotatingCatAndMoveIt(Vector3 distanceVec, float velocity){
+		//we stop rotating ....
+		Quaternion catQuatern = new Quaternion();
+		catQuatern.eulerAngles = new Vector3(0,0,0);
+		// ... and move it ..
+		myRB.velocity = distanceVec*velocity;
+	}
+
+}
